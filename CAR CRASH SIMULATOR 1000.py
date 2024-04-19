b50c25c32682c6d4e4578cb04bb200666bf35f23
Ak228 <93499441+Ak228@users.noreply.github.com>
2024-04-19 12:11:53 -0400

    Update CAR CRASH SIMULATOR 1000.py

--- CAR CRASH SIMULATOR 1000.py
+++ CAR CRASH SIMULATOR 1000.py
@@ -1,3 +1,49 @@
 import arcade
-import tkinter
+from tkinter import *
 import random
+
+class OpenScreen:
+
+    def __init__(self,root):
+        self.root = root
+        root.title("Car Crash Simulator 1000")
+        self.font = ("Times New Roman",20)
+
+        self.topFrame = Frame(self.root)
+        self.topFrame.pack()
+
+        #need welcome label
+        #need start button and level selection
+
+        self.Welcomelabel = Label(self.topFrame, text = 'Welcome to Car Crash Simulator 1000!',
+                            font = self.font)
+        self.Welcomelabel.pack()
+
+        self.LevelLabel = Label(self.topFrame, text = 'Level',font =('Times 18 underline'))
+        self.LevelList = [1,2,3]
+        self.LevelVar = StringVar(self.topFrame)
+        self.LevelVar.set(1)
+        self.Level = OptionMenu(self.topFrame, self.LevelVar, *self.LevelList,)
+
+        self.LevelLabel.pack()
+        self.Level.pack()
+
+        self.StartButton = Button(self.topFrame, text = 'Start')
+        self.StartButton.pack()
+
+        self.InstructionsButton = Button(self.topFrame, text = 'Instructions')
+        self.InstructionsButton.pack()
+
+        #def start():
+
+        #def getinfo():
+
+
+
+
+root = Tk()
+screen = OpenScreen(root)
+root.mainloop()
+
+
+
